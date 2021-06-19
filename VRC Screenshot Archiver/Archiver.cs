720d02180addeaa1d1cf35d6cd46ca9d9fc3f5f6
Edvinas Kulionis <edvis.kulka@gmail.com>
2021-06-19 04:29:54 +0300

    Apply guard clauses to Archiver class

--- VRC Screenshot Archiver/Archiver.cs
+++ VRC Screenshot Archiver/Archiver.cs
@@ -30,96 +30,102 @@ namespace VRC_Screenshot_Archiver
             // The status of the process
             var status = new string[2];
 
-            // If entered directories are valid...
-            if (Directory.Exists(source) && Directory.Exists(destination))
-            {
-                // Reset status
-                OnStatusUpdated(status);
-
-                // Save entered directories to user settings
-                Properties.Settings.Default.SourceDirectory = source;
-                Properties.Settings.Default.DestinationDirectory = destination;
-                Properties.Settings.Default.Save();
-
-                string[] files;
-                try
-                {
-                    // Get the files of the source directory that are likely to be screenshots
-                    files = Directory.GetFiles(source, "*VRChat_*.png");
-                }
-                catch
-                {
-                    status[0] = "Invalid source path.";
-                    OnStatusUpdated(status);
-                    return;
-                }
-
-                status[1] = files.Length + " images found.";
-                OnStatusUpdated(status);
-
-                // If the source directory contains files...
-                if (files.Length != 0)
-                {
-                    status[0] = "0 images moved.";
-                    OnStatusUpdated(status);
-
-                    int moved = 0;
-                    int failed = 0;
-                    foreach (string i in files)
-                    {
-                        // Get the filename with extension
-                        string filename = Path.GetFileName(i);
-
-                        var match = _regex.Match(filename);
-
-                        // If the file is a VRChat screenshot...
-                        if (match.Success)
-                        {
-                            // Prepare the directories for grouping
-                            string dateFolders = string.Empty;
-                            if (settings.HasFlag(Grouping.ByYear))
-                                dateFolders = Path.Combine(dateFolders, $"{match.Groups[3].Value}"); // yyyy
-                            if (settings.HasFlag(Grouping.ByMonth))
-                                dateFolders = Path.Combine(dateFolders, $"{match.Groups[2].Value}"); // yyyy-mm
-                            if (settings.HasFlag(Grouping.ByDay))
-                                dateFolders = Path.Combine(dateFolders, $"{match.Groups[1].Value}"); // yyyy-mm-dd
-
-                            // Create a new directory for the current screenshot (if it does not exist already)
-                            string destPath = Path.Combine(destination, dateFolders, filename);
-                            try
-                            {
-                                Directory.CreateDirectory(Path.Combine(destination, dateFolders));
-                            }
-                            catch
-                            {
-                                failed++;
-                                status[0] = moved + " images moved. " + failed + " failed.";
-                                OnStatusUpdated(status);
-                                continue;
-                            }
-
-                            // Move screenshot to destination
-                            try
-                            {
-                                File.Move(i, destPath);
-                                moved++;
-                            }
-                            catch
-                            { failed++; }
-                            status[0] = moved + " images moved. " + (failed > 0 ? failed + " failed." : "");
-                            OnStatusUpdated(status);
-                        }
-                    }
-                    // Open the destination folder
-                    System.Diagnostics.Process.Start(destination);
-                }
-            }
-            // If entered directories are invalid...
-            else
+            // Check whether entered directories exist
+            if (!Directory.Exists(source) || !Directory.Exists(destination))
             {
                 status[0] = "Invalid path(s).";
                 OnStatusUpdated(status);
-            }    
+                return;
+            }
+
+            // Reset status
+            OnStatusUpdated(status);
+
+            // Save entered directories to user settings
+            Properties.Settings.Default.SourceDirectory = source;
+            Properties.Settings.Default.DestinationDirectory = destination;
+            Properties.Settings.Default.Save();
+
+            // Try to get the files from the source directory that are likely to be screenshots
+            string[] files;
+            try
+            {
+                files = Directory.GetFiles(source, "*VRChat_*.png");
+            }
+            catch
+            {
+                status[0] = "Invalid source path.";
+                OnStatusUpdated(status);
+                return;
+            }
+
+            status[1] = files.Length + " images found.";
+            OnStatusUpdated(status);
+
+            // Check whether the source directory contains files
+            if (files.Length == 0)
+            {
+                return;
+            }
+
+            status[0] = "0 images moved.";
+            OnStatusUpdated(status);
+
+            int moved = 0;
+            int failed = 0;
+            foreach (string i in files)
+            {
+                // Get the filename with extension
+                string filename = Path.GetFileName(i);
+
+                var match = _regex.Match(filename);
+
+                // Check whether the file is a VRChat screenshot
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                // Prepare the directories for grouping
+                string dateFolders = string.Empty;
+                if (settings.HasFlag(Grouping.ByYear))
+                    dateFolders = Path.Combine(dateFolders, $"{match.Groups[3].Value}"); // yyyy
+                if (settings.HasFlag(Grouping.ByMonth))
+                    dateFolders = Path.Combine(dateFolders, $"{match.Groups[2].Value}"); // yyyy-mm
+                if (settings.HasFlag(Grouping.ByDay))
+                    dateFolders = Path.Combine(dateFolders, $"{match.Groups[1].Value}"); // yyyy-mm-dd
+
+                // Try to create a new directory for the current screenshot (if it does not exist already)
+                string destPath = Path.Combine(destination, dateFolders, filename);
+                try
+                {
+                    Directory.CreateDirectory(Path.Combine(destination, dateFolders));
+                }
+                catch
+                {
+                    failed++;
+                    status[0] = moved + " images moved. " + failed + " failed.";
+                    OnStatusUpdated(status);
+                    continue;
+                }
+
+                // Try to move screenshot to destination
+                try
+                {
+                    File.Move(i, destPath);
+                    moved++;
+                }
+                catch
+                {
+                    failed++;
+                
+                }
+                status[0] = moved + " images moved. " + (failed > 0 ? failed + " failed." : "");
+                OnStatusUpdated(status);
+            }
+
+            // Open the destination folder
+            System.Diagnostics.Process.Start(destination);
         }
 
         /// <summary>
